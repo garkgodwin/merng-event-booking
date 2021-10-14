242842517fec5b0baa23e8e2f29dadb99d9fb639
garkgodwin <garkgodwinduque@gmail.com>
2021-10-14 17:06:47 +0800

    Added seed file

--- server/index.js
+++ server/index.js
@@ -3,25 +3,28 @@ const db = require("./models");
 require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 const ATLAS_URI = process.env.ATLAS_URI;
+const { createAdmin } = require("./seed.js");
 
 const main = async () => {
   //*DATABASE
   /*
     if database is started, start routes?, if routes started start server
   */
-  db.mongoose
+  console.log("> Connecting to Mongo DB...");
+  await db.mongoose
     .connect(ATLAS_URI)
-    .then(() => {
-      console.log("Connected to Mongo DB!");
+    .then(async () => {
+      console.log("=> Connected to Mongo DB.");
+      await createAdmin();
       app.get("/", (req, res) => {
         res.send("HELLO WORLD!");
       });
       app.listen(PORT, () => {
-        console.log(`Server listening on port ${PORT}`);
+        console.log(`=> Server listening on port ${PORT}`);
       });
     })
     .catch((error) => {
-      console.log(error);
+      console.log("!> " + error);
     });
 };
 
