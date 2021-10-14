c15af5b5ca9ba846cc63947ec476b8fc425270ed
garkgodwin <garkgodwinduque@gmail.com>
2021-10-14 21:47:54 +0800

    Created booking typedefs

--- server/graphql/typedefs/index.js
+++ server/graphql/typedefs/index.js
@@ -3,22 +3,26 @@ const { buildSchema } = require("graphql");
 //*types
 const typeUser = require("./user.type");
 const typeEvent = require("./event.type");
+const typeBooking = require("./booking.type");
 //?_common typedef
 const typeCommon = require("./_common.type");
 
 var schema = buildSchema(`
-${typeUser.types}
-    ${typeEvent.types}
     ${typeCommon.types}
+    ${typeUser.types}
+    ${typeEvent.types}
+    ${typeBooking.types}
     
 type RootQuery {
     ${typeEvent.queries}
     ${typeUser.queries}
+    ${typeBooking.queries}
 }
 
 type RootMutation {
     ${typeEvent.mutations}
     ${typeUser.mutations}
+    ${typeBooking.mutations}
 }
 
 schema {
