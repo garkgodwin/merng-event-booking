245c495daf452ebda4e8d6c11de7f0b27830658c
garkgodwin <garkgodwinduque@gmail.com>
2021-10-14 15:04:13 +0800

    Updated user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,26 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = Schema({
+  type: {
+    type: Number,
+    min: [1, "Out of range"],
+    max: [3, "Out of range"],
+    required: "User type is required.",
+  },
+  name: new Schema({
+    first: {
+      type: String,
+      required: "First name is required.",
+    },
+    middle: String,
+    last: {
+      type: String,
+      required: "Last name is required.",
+    },
+    preffix: String,
+    suffix: String,
+    titles: [String],
+  }),
   email: {
     type: String,
     required: true,
