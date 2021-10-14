1afa82ee366825bda42cc2e46336bb42690cff88
garkgodwin <garkgodwinduque@gmail.com>
2021-10-14 15:07:35 +0800

    Updated user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -2,6 +2,7 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = Schema({
   userType: {
+    //?usertype === 1 is admin | 2 is event managers | 3 is customer
     type: Number,
     min: [1, "Out of range"],
     max: [3, "Out of range"],
