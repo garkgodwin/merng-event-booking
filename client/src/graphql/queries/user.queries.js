b743257063f2cadb7ed8c5af50fbfd360141d9a4
garkgodwin <garkgodwinduque@gmail.com>
2021-10-19 12:22:24 +0800

    Updated query

--- client/src/graphql/queries/user.queries.js
+++ client/src/graphql/queries/user.queries.js
@@ -3,26 +3,19 @@ import { gql } from "@apollo/client";
 export const GET_LOGGED_IN_DATA = gql`
   query {
     getLoggedInData {
-      success
-      invalid
-      error
-      message
-      errors
-      user {
-        _id
-        userType
-        name {
-          first
-          middle
-          last
-        }
-        email
-        createdEvents {
-          _id
-        }
-        createdAt
-        updatedAt
+      _id
+      userType
+      name {
+        first
+        middle
+        last
       }
+      email
+      createdEvents {
+        _id
+      }
+      createdAt
+      updatedAt
     }
   }
 `;
