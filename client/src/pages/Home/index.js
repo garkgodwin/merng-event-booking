c01e337da3a7f2770f033d618dcb155d56b00487
garkgodwin <garkgodwinduque@gmail.com>
2021-10-21 14:07:01 +0800

    Added get events

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
+import { GET_EVENTS } from "../../graphql/queries/event.queries";
+
 //?COMPONENTS
 import EventCard from "../../components/cards/EventCard";
 
 const Home = () => {
+  const { error, loading, data } = useQuery(GET_EVENTS);
+  console.log("DATA: ");
+  console.log(data);
+  console.log("ERROR: " + error);
+  console.log("LOADING: " + loading);
   return (
     <div className="Page page-home">
       <EventCard />
--- server/graphql/resolvers/index.js
+++ server/graphql/resolvers/index.js
@@ -6,11 +6,15 @@ const {
   //?MUTATIONS
   createUser,
 } = require("./user.resolvers");
+
+const { getEvents } = require("./event.resolvers");
+
 const resolvers = {
   Query: {
     getAllUsers,
     getLoggedInData,
     login,
+    getEvents,
   },
   Mutation: {
     createUser,
